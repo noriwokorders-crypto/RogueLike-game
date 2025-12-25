8aea2427927cc8cd2ff0c837628907fbcde4e8a7
noriwokorders-crypto <noriwokorders@gmail.com>
2025-12-25 23:48:16 +0200

    Add files via upload

---
+++ Sheep_Grass.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Sheep_Grass" tilewidth="128" tileheight="128" tilecount="12" columns="12">
+ <image source="Sheep_Grass.png" width="1536" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+   <frame tileid="8" duration="150"/>
+   <frame tileid="9" duration="150"/>
+   <frame tileid="10" duration="150"/>
+   <frame tileid="11" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Tower_Destroyed.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tower_Destroyed" tilewidth="125" tileheight="256" tilecount="1" columns="1">
+ <image source="Tower_Destroyed.png" width="128" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="4.66667" y="153.333" width="119.667" height="70.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
