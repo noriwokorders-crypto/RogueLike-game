667b018bf31a182c669dd5bd0d375eab8e38498b
noriwokorders-crypto <noriwokorders@gmail.com>
2025-12-25 16:31:55 +0200

    Add files via upload

---
+++ 06.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="06" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="06.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="14.6667" y="27" width="40" height="18.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ Tree1.tsx
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tree1" tilewidth="192" tileheight="256" tilecount="8" columns="8">
+ <image source="Tree1.png" width="1536" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="6" x="45" y="188" width="105.667" height="24"/>
+   <object id="7" x="82" y="213" width="37.3333" height="23.3333"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="41.3333" y="183" width="112" height="30"/>
+   <object id="2" x="80.3333" y="213.333" width="38.6667" height="21.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="41" y="186" width="112" height="28"/>
+   <object id="2" x="82.6667" y="214.333" width="33.6667" height="20.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="40.3333" y="187" width="113" height="24.6667"/>
+   <object id="2" x="82.3333" y="213" width="35.3333" height="22.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="40.6667" y="186" width="111.667" height="26.6667"/>
+   <object id="2" x="81.3333" y="213.333" width="37.3333" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="41.6667" y="186.333" width="107.333" height="27.6667"/>
+   <object id="2" x="85.3333" y="215" width="32.6667" height="21"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="36.6667" y="188" width="115" height="26.6667"/>
+   <object id="2" x="82" y="216" width="35.3333" height="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="43" y="182.667" width="105.667" height="32"/>
+   <object id="2" x="80.6667" y="216.667" width="38.3333" height="18.3333"/>
+  </objectgroup>
+ </tile>
+</tileset>
