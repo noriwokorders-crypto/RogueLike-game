2b22c8525486ba5109598b0263d4c7c64aba728c
noriwokorders-crypto <noriwokorders@gmail.com>
2025-12-26 00:29:09 +0200

    Add files via upload

---
+++ Tree2.tsx
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tree2" tilewidth="192" tileheight="256" tilecount="8" columns="8">
+ <image source="Tree2.png" width="1536" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="56" y="186" width="81" height="27"/>
+   <object id="2" x="84" y="215" width="32" height="27"/>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+   <frame tileid="4" duration="150"/>
+   <frame tileid="5" duration="150"/>
+   <frame tileid="6" duration="150"/>
+   <frame tileid="7" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="45" y="180" width="100" height="35"/>
+   <object id="2" x="81" y="218" width="33" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="47" y="182" width="100" height="32"/>
+   <object id="2" x="82" y="218" width="33" height="23"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="45" y="179" width="102" height="34"/>
+   <object id="2" x="85" y="218" width="33" height="27"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="47" y="180" width="100" height="32"/>
+   <object id="2" x="83" y="214" width="35" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="48" y="191" width="97" height="22"/>
+   <object id="2" x="82" y="215" width="35" height="26"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="45" y="190" width="105" height="26"/>
+   <object id="2" x="84" y="218" width="35" height="19"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="43" y="181" width="107" height="33"/>
+   <object id="2" x="79" y="212" width="42" height="30"/>
+  </objectgroup>
+ </tile>
+</tileset>
