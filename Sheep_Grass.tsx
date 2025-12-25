9ac836c10c11404efd6f952d86b20bc26004c0ff
noriwokorders-crypto <noriwokorders@gmail.com>
2025-12-25 23:47:21 +0200

    Delete Sheep_Grass.tsx

--- Sheep_Grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Sheep_Grass" tilewidth="128" tileheight="128" tilecount="12" columns="12">
- <image source="Sheep_Grass.png" width="1536" height="128"/>
-</tileset>
